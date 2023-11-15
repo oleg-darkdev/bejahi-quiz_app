9a3de9435c2f1881ac883647d97b281c6c120bd6
DarkDev <darkdev_brand@riseup.net>
2023-11-15 12:57:29 +0200

    upd text

--- src/lib/widgets/ui/landing/Footer.svelte
+++ src/lib/widgets/ui/landing/Footer.svelte
@@ -1,14 +1,10 @@
-<footer class="bg-black" >
-
+<footer class="bg-black">
 	<div class="mx-auto max-w-3xl bg-gray-200 px-4 py-12 sm:px-6 lg:px-16">
 		<div class="flex flex-wrap items-baseline lg:justify-center">
 			<span class="mt-2 text-sm font-light text-gray-500">
-				Copyright © 2020 - 2021
-				<a
-					href="https://wickedlabs.dev"
-					class="text-wickedblue mx-2 hover:text-gray-500"
-					rel="noopener noreferrer">@wickedlabsHQ</a
-				>. Since 2020
+				Copyright © 2021 - 2023
+				<p class=" mx-2 hover:text-gray-500">©Valentin Dragin & Leonid Levin History Workshop.</p>
+				<p class=" mx-2 hover:text-gray-500">2021, All Rigths Reserved.</p>
 			</span>
 		</div>
 	</div>
